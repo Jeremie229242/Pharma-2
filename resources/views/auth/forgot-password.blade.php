7406669dda08320a3862d7bb6debfceb15d468eb
unknown <jeremie229242@gmail.com>
2025-10-02 10:04:44 +0100

    Initial  commit

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,105 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+
+@extends("layouts.applog")
+@section('title','PRO-PHARMA | LOGIN')
+@section("contenu")
+
+        <div class="container mx-auto px-4  h-full">
+    <div class="flex content-center items-center justify-center h-full">
+
+      <div class="w-full  px-4">
+        <div
+          class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0"
+        >
+          <div class="rounded-t mb-0 px-6 py-6">
+            <div class="text-center mb-3">
+              <h6 class="text-blueGray-500 text-sm font-bold">
+                Verification Email
+              </h6>
+            </div>
+
+            <hr class="mt-6 border-b-1 border-blueGray-300" />
+          </div>
+          <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
+            <div class="text-blueGray-400 text-center mb-3 font-bold">
+              <small>        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+              </small>
+            </div>
+            <form method="POST" action="{{ route('password.email') }}">
+            @csrf
+              <div class="relative w-full mb-3">
+                <label
+                  class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                  htmlFor="grid-password"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                  placeholder="Votre Email"
+                  name="email"  required autofocus
+                />
+              </div>
+
+              <div class="relative w-full mb-3">
+                <label
+                  class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                  htmlFor="grid-password"
+                >
+                  Password
+                </label>
+                <input
+                  type="password"
+                   name="password"
+                                required autocomplete="current-password"
+                  class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                  placeholder="Mot de Passe"
+                />
+              </div>
+              <div>
+                <label class="inline-flex items-center cursor-pointer">
+                  <input
+                    id="customCheckLogin"
+                    type="checkbox"
+                    name="remember"
+                    class="form-checkbox border-0 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150"
+                  />
+                  <span class="ml-2 text-sm font-semibold text-blueGray-600">
+                    Se Souvenir de moi
+                  </span>
+                </label>
+              </div>
+
+              <div class="text-center mt-6">
+                <button
+                  class="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+                  type="submit"
+                >
+                  Sign In
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div class="flex flex-wrap mt-6 relative">
+          <div class="w-1/2">
+            <a href="{{ route('password.request') }}" class="text-blueGray-200">
+              <small>Mot de passe oubliée?</small>
+            </a>
+          </div>
+          <div class="w-1/2 text-right">
+            <a href="{{ route('register') }}"  class="text-blueGray-200">
+              <small>Créer un Nouveau Compte</small>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
+  @endsection
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+
+
